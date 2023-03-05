83e121ff5fae4a12b47731716005ab25da07d1fa
martinaCodes <martina.van.earth@gmail.com>
2023-03-05 12:01:02 +0100

    Added NativeIFC presentation

--- presentations/js/config.js
+++ presentations/js/config.js
@@ -27,7 +27,7 @@ export default {
 
 	// Visibility rule for backwards navigation arrows; "faded", "hidden"
 	// or "visible"
-	controlsBackArrows: 'hidden',
+	controlsBackArrows: 'faded',
 
 	// Display a presentation progress bar
 	progress: true,
@@ -64,6 +64,9 @@ export default {
 
 	// Flags if we should monitor the hash and change slides accordingly
 	respondToHashChanges: true,
+
+	// Enable support for jump-to-slide navigation shortcuts
+	jumpToSlide: true,
 
 	// Push each slide change to the browser history.  Implies `hash: true`
 	history: false,
@@ -283,6 +286,10 @@ export default {
 
 	// Time before the cursor is hidden (in ms)
 	hideCursorTime: 5000,
+
+	// Should we automatmically sort and set indices for fragments
+	// at each sync? (See Reveal.sync)
+	sortFragmentsOnSync: true,
 
 	// Script dependencies to load
 	dependencies: [],
